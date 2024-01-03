3cd2a523235e991dddcfa3a64eab0b63dfb23583
JiangXsong <xu89219@gmail.com>
2024-01-03 16:20:36 +0800

    first commit

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import os
-
+import numpy as np
 import librosa
 
 data_dir=""
@@ -9,13 +9,18 @@ Out_dir="data"
 
 def preprocess_one_dir(in_dir, out_dir, out_filename, sample_rate=16000):
     file_infos = []
+    data = []
+    parameter = []
     in_dir = os.path.abspath(in_dir)
     wav_list = os.listdir(in_dir)
     for wav_file in wav_list:
         if not wav_file.endswith('.wav'):
             continue
-        wav_path = os.path.join(in_dir, wav_file)
+        wav_path = os.path.join(in_dir, wav_file)       
         samples, _ = librosa.load(wav_path, sr=sample_rate)
+        sp, yphase = Spectrum(samples, 512, 256, 512, 2)
+        data.append(sp)
+        parameter.append([yphase, wav_file.replace(wav_list, '')])
         file_infos.append((wav_path, len(samples)))
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
@@ -31,6 +36,38 @@ def preprocess(args):
                                speaker,
                                sample_rate=args.sample_rate)
 
+def Spectrum(sig, FrameLength, FrameRate, FFT_SIZE, flag):       #(sig, 512, 256, 512, 2)
+    Len = len(sig)
+    ncols = int((Len-FrameLength)/FrameRate)
+    fftspectrum = np.zeros([FFT_SIZE, ncols])
+    Spectrum = np.zeros([FFT_SIZE//2+1, ncols])
+    En = np.zeros([1, ncols])
+    wind = np.hamming(FrameLength)
+    
+    x_seg = []
+    fftspectrum = []
+    yphase = []
+    Spec = []
+    i = 0
+    for t in range(0, Len-FrameLength, FrameRate):
+        #pdb.set_trace()
+        x_seg.append(wind*(sig[t:(t+FrameLength)]))
+        fftspectrum.append(np.fft.fft(x_seg[i], FFT_SIZE))
+        yphase.append(np.angle(fftspectrum[i]))
+        Spec.append(np.abs(fftspectrum[i][0:FFT_SIZE//2+1]))
+        i += 1
+    
+    fftspectrum = np.array(fftspectrum)
+    yphase = np.array(yphase)
+    Spec = np.array(Spec)
+    #pdb.set_trace()
+    if flag==2:
+        Spec = Spec**2
+    elif flag==1:
+        Spec = Spec
+    else:
+        Spec = fftspectrum[0:FFT_SIZE//2, :]
+    return np.log10(Spec, where=Spec>0), yphase
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("data preprocessing")
