d79702f49c41be10964c47594fdb498744de5131
JiangXsong <xu89219@gmail.com>
2024-03-13 16:15:33 +0800

    lab212

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -8,26 +8,26 @@ from data_tools import audio_to_numpy, clean_file_to_matrix, numpy_audio_to_matr
 data_dir = "/home/song/LS-UNet/"
 Out_dir = "data"
 
-def prepare_one_dir(data_type, out_dir, snr_list):
+def prepare_one_dir(data_type, in_dir, out_dir, snr_list, sample_rate, frame_length, n_fft):
   noise_data_list = []
   if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
   for snr in snr_list:      
-    noise_data_list.append(audio_to_numpy(os.path.join(args.in_dir, data_type, snr),
-                                          sample_rate=args.sample_rate,
-                                          frame_length=args.frame_length))    
+    noise_data_list.append(audio_to_numpy(os.path.join(in_dir, data_type, snr),
+                                          sample_rate=sample_rate,
+                                          frame_length=frame_length))    
   noise_data_array = np.vstack(noise_data_list)
-  dim_square_spec = int(args.n_fft/2)+1
+  #dim_square_spec = int(args.n_fft/2)+1
 
-  sp_noise, yphase = numpy_audio_to_matrix_spectrogram(noise_data_array, args.n_fft, hop_length_fft=18)
+  sp_noise, yphase = numpy_audio_to_matrix_spectrogram(noise_data_array, n_fft, hop_length_fft=18)
     
   np.save(out_dir + 'noise_Spec', sp_noise)    
   np.save(out_dir + 'noise_Phase', yphase)
 
-  clean_path = os.path.join(args.in_dir, data_type, 'clean')
+  clean_path = os.path.join(in_dir, data_type, 'clean')
   if os.path.exists(clean_path):
-    sp_clean = clean_file_to_matrix(os.path.join(args.in_dir, data_type, 'clean'), len(snr_list))
+    sp_clean = clean_file_to_matrix(os.path.join(in_dir, data_type, 'clean'), len(snr_list))
     np.save(out_dir + 'clean', sp_clean)
 
 def prepare_data(args):
@@ -36,10 +36,10 @@ def prepare_data(args):
 
     if data_type == 'tr':
       snr_list = ['-2dB', '-6dB', '2dB', "6dB"]
-      prepare_one_dir(out_dir, snr_list)
+      prepare_one_dir(data_type, args.in_dir, out_dir, snr_list, args.sample_rate, args.frame_length, args.n_fft)
 
     elif data_type == 'cv':
-      snr_list = ['-2dB', '-6dB', '2dB', "6dB"]
+      snr_list = ['-5dB']
       prepare_one_dir(data_type, out_dir, snr_list)
 
     else:
